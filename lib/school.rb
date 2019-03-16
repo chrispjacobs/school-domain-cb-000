3205231dad94573db8fbb76175a983a578602781
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-16 21:28:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def name
+    @name
+  end
+  def add_student(name, grade)
+    @roster[grade] = name
+end
