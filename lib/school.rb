9c794d6acdbadac9b307bbb809fb45463539fc6d
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-16 21:29:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,4 +8,13 @@ class School
   end
   def add_student(name, grade)
     @roster[grade] = name
+  end
+  def roster
+    @roster
+  end
 end
+
+wellington = School.new("Wellington High School")
+puts wellington.name
+wellington.add_student("Chris Jacobs", 11)
+puts
