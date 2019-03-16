a2281aaf1cae96cb0357c7b1377afd2c6dc00d14
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-16 22:59:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,6 +9,7 @@ class School
   def add_student(name, grade)
     if @roster[grade] != nil
        @roster[grade] << name
+       @roster[grade].sort
     else
       @roster[grade] = [name]
     end
